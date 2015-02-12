29f326a5b629e21d5056d086789d5dad17a3c03d
Corentin Branquet <corentin.branquet@gmail.com>
2015-02-12 12:26:05 +0100

    Push Workflow front-end

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Project title</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+	
+	<!-- Favicon -->
+
+
+	<!-- Google Fonts -->
+	
+
+	<!-- FontAwesome -->
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="bower_components/gridiculous/gridiculous.css">
+	<link rel="stylesheet" href="bower_components/normalize.css/normalize.css">
+
+	<link rel="stylesheet" href="stylesheets/style.css">
+
+
+	<!-- Scripts -->
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+
+
+	<!-- Google map API -->
+
+</head>
+
+
+<body>
+
+<div id="wrapper">
+	<h1>Ready to Go !</h1>
+</div>
+
+</body>
+
+
+</html>
